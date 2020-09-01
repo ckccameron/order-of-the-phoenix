979c321b2194bc850f70abf8f74264d2839fd206
Cam C <ckc.cameron@gmail.com>
2020-09-01 11:09:38 -0600

    Create Wizard PORO

---
+++ app/poros/wizard.rb
@@ -0,0 +1,13 @@
+class Wizard
+  attr_reader :name,
+              :role,
+              :house,
+              :patronus
+
+  def initialize(info)
+    @name = info[:name]
+    @role = info[:role]
+    @house = info[:house]
+    @patronus = info[:patronus]
+  end
+end
