0457240f836a659326b8cbaa974cfe8bd4fd851e
Cam C <ckc.cameron@gmail.com>
2020-09-01 11:06:41 -0600

    Setup characters conversio into objects

--- app/services/harry_potter_service.rb
+++ app/services/harry_potter_service.rb
@@ -1,6 +1,18 @@
 class HarryPotterService
+  def house_members
+    json("/characters")
+  end
+
+  def object_conversion
+    characters.map do |character|
+      Wizard.new(character)
+    end
+  end
+
+  private
+
   def conn
-    Faraday.new("https://www.potterapi.com/v1/characters") do |f|
+    Faraday.new("https://www.potterapi.com/v1") do |f|
       f.params["key"] = ENV["HP_API_KEY"]
   end
 
