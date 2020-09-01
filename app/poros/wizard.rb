66d49819577065b7234478d4323e60e3106f341e
Cam C <ckc.cameron@gmail.com>
2020-09-01 11:31:48 -0600

    Add id to Wizard PORO attributes

--- app/poros/wizard.rb
+++ app/poros/wizard.rb
@@ -2,12 +2,15 @@ class Wizard
   attr_reader :name,
               :role,
               :house,
-              :patronus
+              :patronus,
+              :id
 
   def initialize(info)
+    binding.pry
     @name = info[:name]
     @role = info[:role]
     @house = info[:house]
     @patronus = info[:patronus]
+    @id = info[:id]
   end
 end
